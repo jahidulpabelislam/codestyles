340beb94c1dccfb09c0e54cf9e5b6d8ccc3d7672
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2020-02-03 20:28:37 +0000

    Don't make only function call be chained!

--- browser.eslint.config.js
+++ browser.eslint.config.js
@@ -93,7 +93,7 @@ module.exports = {
         "newline-per-chained-call": [
             "warn",
             {
-                ignoreChainWithDepth: 1,
+                ignoreChainWithDepth: 2,
             },
         ],
         "no-array-constructor": "error",
--- node.eslint.config.js
+++ node.eslint.config.js
@@ -114,7 +114,7 @@ module.exports = {
         "newline-per-chained-call": [
             "warn",
             {
-                ignoreChainWithDepth: 1,
+                ignoreChainWithDepth: 2,
             },
         ],
         "no-array-constructor": "error",
